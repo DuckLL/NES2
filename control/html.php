1fbca1b62f2af30c6889a8685f28ab0b86c3f8c3
duckll <a347liao@gmail.com>
2015-02-01 11:35:05 +0800

    nav

--- control/html.php
+++ control/html.php
@@ -1,38 +1,43 @@
 <?php 
-echo'	
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>國立嘉義大學社團活動系統</title>
-		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-		<link href="css/starter-template.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<script src="js/jquery-2.1.1.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">國立嘉義大學社團活動系統</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand">國立嘉義大學社團活動系統</a>
-				</div>
-				<div class="navbar-collapse collapse" style="height: 1px;">
-					<ul class="nav navbar-nav">
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div>
-		</div>
+function head(){
+echo'
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>國立嘉義大學社團活動系統</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="css/starter-template.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<script src="js/jquery-2.1.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 		<div class="container">
-			<div class="starter-template">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">國立嘉義大學社團活動系統</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand">國立嘉義大學社團活動系統</a>
 			</div>
+			<div class="navbar-collapse collapse" style="height: 1px;">
+				<ul class="nav navbar-nav">';
+				}
+				function tail(){
+					echo'
+				</ul>
+			</div><!--/.nav-collapse -->
 		</div>
-	</body>
-	</html>';
+	</div>
+	<div class="container">
+		<div class="starter-template">
+		</div>
+	</div>
+</body>
+</html>';
+}
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php 
 require_once('control/html.php');
+head();
+?>
+<li><a href="index.php">公告</a></li>
+<?php
+tail();
 ?>
