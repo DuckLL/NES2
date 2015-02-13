a462860ed59eb437a67851303ed4e20e283c632b
duckll <a347liao@gmail.com>
2015-02-13 10:39:21 +0800

    login update

--- control/login.php
+++ control/login.php
@@ -6,7 +6,7 @@ if(!isset($_POST['account'])||!isset($_POST['password'])){
 }
 if(!preg_match("/^[a-zA-Z]\w*$/",$_POST['account'])){
 	echo "<script type=\"text/javascript\">";
-	echo 'alert("您的ID格式不正確 第一個字不為數字，只接受大小寫字母、數字及底線");';
+	echo 'alert("帳號或密碼錯誤!");';
 	echo "history.go(-1);";
 	echo "</script>";
 	exit;
